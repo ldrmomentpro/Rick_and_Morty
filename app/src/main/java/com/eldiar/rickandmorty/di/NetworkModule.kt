057b5c95453b2dd0cfa49a12c2faa77323cbe42d
Eldiar <73545742+ldrmomentpro@users.noreply.github.com>
2022-04-18 12:27:02 +0600

    create networkModule.kt

---
+++ app/src/main/java/com/eldiar/rickandmorty/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package com.eldiar.rickandmorty.di
+
+import com.eldiar.rickandmorty.data.network.CharactersApi
+import com.eldiar.rickandmorty.data.network.CharactersApi.Companion.BASE_URL
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+    single<Retrofit> { provideRetrofit() }
+    single<CharactersApi> { provideServiceApi(get()) }
+}
+
+fun provideRetrofit(): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
+
+fun provideServiceApi(retrofit: Retrofit): CharactersApi =
+    retrofit.create(CharactersApi::class.java)
